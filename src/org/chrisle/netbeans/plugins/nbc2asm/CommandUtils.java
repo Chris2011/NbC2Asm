a2c9466d26ec711daa3514fc0139e23a39bb9e7b
Chris2011 <Christian.Lenz@gmx.net>
2015-06-29 22:24:04 +0200

    Added logic to open asm file after compiling.

--- src/org/chrisle/netbeans/plugins/nbc2asm/CommandUtils.java
+++ src/org/chrisle/netbeans/plugins/nbc2asm/CommandUtils.java
@@ -1,10 +1,15 @@
 package org.chrisle.netbeans.plugins.nbc2asm;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import org.netbeans.api.extexecution.ExecutionDescriptor;
 import org.netbeans.api.extexecution.ExecutionService;
+import org.openide.cookies.OpenCookie;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
 import org.openide.util.Exceptions;
 
 public class CommandUtils {
@@ -12,18 +17,27 @@ public class CommandUtils {
         try {
             // Show something as Outputwindow Caption
             Runtime runtime = Runtime.getRuntime();
-            final Process process = runtime.exec("gcc -S " + inputFile + " -o " + outputFile);
+            Process process = runtime.exec("gcc -S " + inputFile + " -o " + outputFile);
 
             Callable processCallable = (Callable) () -> process;
-
             ExecutionDescriptor descriptor = new ExecutionDescriptor().frontWindow(true).controllable(true).showSuspended(true);
-
             ExecutionService service = ExecutionService.newService(processCallable, descriptor, tabName);
 
-            Future task = service.run();
-            if (task.isDone()) {
-                // TODO: Show success message.;
+            Future<Integer> task = service.run();
+
+            try {
+                if (task.get() == 0) {
+                    // TODO: Message if it's done, inside the same output tab.;
+//                    InputOutput test = IOProvider.get(tabName).getIO(tabName, null);
+//                    test.getOut().println("Done.");
+                    
+                    File asmFile = new File(outputFile);
+                    DataObject.find(FileUtil.toFileObject(asmFile)).getLookup().lookup(OpenCookie.class).open();
+                }
+            } catch (InterruptedException | ExecutionException ex) {
+                Exceptions.printStackTrace(ex);
             }
+            
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
